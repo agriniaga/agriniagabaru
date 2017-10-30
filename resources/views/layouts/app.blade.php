b99b945cda0288a8628c6dde6cebaf79e3c3c59b
jack <hdrv.executions@gmail.com>
2017-10-30 17:47:53 +0700

    try to merge 2.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,12 +46,6 @@
 @yield('content')
 <footer class="main-footer">
 </footer>
-=======
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
->>>>>>> ba3aa178c512ec0a7898a371dd98931292a87345
 
                     <!-- Collapsed Hamburger -->
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
@@ -61,7 +55,6 @@
                         <span class="icon-bar"></span>
                     </button>
 
-<<<<<<< HEAD
 <script src="{{ url('/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="{{ url('/bootstrap/js/bootstrap.min.js') }}"></script>
@@ -92,56 +85,5 @@
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!-- Select2 -->
 <script src="{{ url('/plugins/select2/select2.full.min.js') }}"></script>
-=======
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
-    </div>
-
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
->>>>>>> ba3aa178c512ec0a7898a371dd98931292a87345
 </body>
 </html>
